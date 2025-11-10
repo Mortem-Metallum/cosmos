f5a53ea23b930dd2776336edf05e6ba7d6618d1b
John Doe <hanszimmer7171@proton.me>
2025-11-10 11:53:37 -0500

    Update cosmos.c

--- cosmos.c
+++ cosmos.c
@@ -8,20 +8,24 @@ int main(int argc, char *argv[]){
         if(strcmp(argv[i], "install") == 0){
             char input[50];
             char cmd1[1024];
-            snprintf(cmd1, sizeof(cmd1), "wget https://github.com/Mortem-Metallum/cosmos-repo/%s.tar.gz", input);
+
             printf("install> ");
-            scanf("%s", &input);
-            printf("I >>> Executing command: wget %s", input);
+            scanf("%49s", input);  // read user input safely
+
+            snprintf(cmd1, sizeof(cmd1), "wget https://github.com/Mortem-Metallum/cosmos-repo/%s.tar.gz", input);
+            printf("I >>> Executing command: wget %s\n", input);
+
             int e = system(cmd1);
             if(e != 0){
-                printf("E >>> Failed! %d", e);
-                printf("The operation could not complete due to previous errors");
+                printf("E >>> Failed! %d\n", e);
+                printf("The operation could not complete due to previous errors\n");
             } 
         } else {
-            printf("E >>> Missing operand");
-            printf("I >>> Try cosmos --help for more information");
+            printf("E >>> Missing operand\n");
+            printf("I >>> Try cosmos --help for more information\n");
             return 1;
         }
+
         i++;
     }
     return 0;
