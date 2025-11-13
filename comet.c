6a8ecae786e924606a91c7f5dd5cc8542cafc857
John Doe <hanszimmer7171@proton.me>
2025-11-13 13:22:53 -0500

    Update comet.c

--- comet.c
+++ comet.c
@@ -1,30 +1,65 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 
-int main(int argc, char *argv[]){
-  int i = 1;
-  while(i < argc){
-    if(strcmp(argv[i], "-d") == 0){
-      char builddir[100];
-      char compilecmd[350];
+#define MAX_LINE 256
 
-      
-      strncpy(builddir, argv[2], sizeof(builddir) - 1);
-      builddir[sizeof(builddir) - 1] = '\0';
-
-      snprintf(compilecmd, sizeof(compilecmd), "make -C %s -j$(nproc)", builddir);
-      printf("COMET: Running compile command\n");
-      printf(":: %s\n", compilecmd);
-      int r1 = system(compilecmd);
-      if(r1 != 0){
-        printf("COMET: Failed!");
+int main(int argc, char *argv[]) {
+    if(argc < 2){
+        printf("Usage: comet -d <builddir>\n");
         return 1;
-      }
-      printf("COMET: Finished compiling: %s\n", builddir);
-      return 0;
     }
-  }
-  i++;
-  return 0;
+
+    char builddir[100];
+    strncpy(builddir, argv[2], sizeof(builddir)-1);
+    builddir[sizeof(builddir)-1] = '\0';
+
+    char ini_path[150];
+    snprintf(ini_path, sizeof(ini_path), "%s/buildconf.ini", builddir);
+
+    FILE *ini = fopen(ini_path, "r");
+    if(!ini){
+        printf("COMET: No buildconf.ini, using default make command\n");
+    }
+
+    char line[MAX_LINE];
+    char depends[MAX_LINE] = "NONE";
+    char makeflags[MAX_LINE] = "make -j$(nproc)";
+    char version[MAX_LINE] = "UNKNOWN";
+
+    if(ini){
+        while(fgets(line, sizeof(line), ini)){
+            line[strcspn(line, "\r\n")] = 0;
+
+            if(line[0] == '#' || line[0] == ';' || line[0] == '\0') continue;
+
+            if(strncmp(line, "depends", 7) == 0){
+                sscanf(line, "depends = %255[^\n]", depends);
+            } else if(strncmp(line, "makeflags", 9) == 0){
+                sscanf(line, "makeflags = %255[^\n]", makeflags);
+            } else if(strncmp(line, "version", 7) == 0){
+                sscanf(line, "version = %255[^\n]", version);
+            }
+        }
+        fclose(ini);
+    }
+
+    printf("COMET: BuildDir=%s\n", builddir);
+    printf("COMET: Depends=%s\n", depends);
+    printf("COMET: Version=%s\n", version);
+    printf("COMET: MakeFlags=%s\n", makeflags);
+
+    char compilecmd[512];
+    snprintf(compilecmd, sizeof(compilecmd), "%s -C %s", makeflags, builddir);
+    printf("COMET: Running compile command\n:: %s\n", compilecmd);
+
+    int r = system(compilecmd);
+    if(r != 0){
+        printf("COMET: Failed!\n");
+        return 1;
+    }
+    printf("COMET: Finished compiling: %s\n", builddir);
+
+    return 0;
 }
+
