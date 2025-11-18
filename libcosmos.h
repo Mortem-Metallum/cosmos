ed43f844b6296d92603e0a1f4054c4a69e6a11c3
John Doe <hanszimmer7171@proton.me>
2025-11-18 13:15:01 -0500

    Update libcosmos.h

--- libcosmos.h
+++ libcosmos.h
@@ -2,10 +2,10 @@
 #define LIBCOSMOS_H
 #define MAX_LINE 256
 
-char line[MAX_LINE];
-char installto[MAX_LINE];
-char depends[MAX_LINE];
-char makeflags[MAX_LINE];
-char version[MAX_LINE];
+extern char line[MAX_LINE];
+extern char installto[MAX_LINE];
+extern char depends[MAX_LINE];
+extern char makeflags[MAX_LINE];
+extern char version[MAX_LINE];
 
 #endif
