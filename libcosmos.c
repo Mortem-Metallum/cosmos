614be71306b0198bf524dc16584e3364368ef6ac
John Doe <hanszimmer7171@proton.me>
2025-11-18 13:11:45 -0500

    Create libcosmos.c

---
+++ libcosmos.c
@@ -0,0 +1,11 @@
+#include "libcosmos.h"
+
+
+installto[MAX_LINE] = "/usr/bin";
+depends[MAX_LINE] = "NONE";
+makeflags[MAX_LINE] = "make -j$(nproc)";
+version[MAX_LINE] = "UNKNOWN";
+
+int main(){
+  return 0;
+}
