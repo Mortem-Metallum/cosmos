dbc545e3dffd289d95c4fc35dba81af68c9b7327
John Doe <hanszimmer7171@proton.me>
2025-11-18 13:20:56 -0500

    Update libcosmos.c

--- libcosmos.c
+++ libcosmos.c
@@ -5,7 +5,3 @@ char installto[MAX_LINE] = "/usr/bin";
 char depends[MAX_LINE] = "NONE";
 char makeflags[MAX_LINE] = "make -j$(nproc)";
 char version[MAX_LINE] = "UNKNOWN";
-
-int main(){
-  return 0;
-}
