833a828688d039d49013aa2b971fdc87a7449242
John Doe <hanszimmer7171@proton.me>
2025-11-18 12:52:46 -0500

    Update comet.c

--- comet.c
+++ comet.c
@@ -71,7 +71,7 @@ int main(int argc, char *argv[]) {
         
     }
     
-    print("COMET: Installing: %s>=%s", builddir, version);
+    printf("COMET: Installing: %s>=%s", builddir, version);
 
     char compilecmd[512];
     snprintf(compilecmd, sizeof(compilecmd), "%s -C %s", makeflags, builddir);
