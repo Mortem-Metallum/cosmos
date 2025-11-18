93f4fc6cec93fa7d798aacadcb6ebe0e84d4f77f
John Doe <hanszimmer7171@proton.me>
2025-11-18 13:58:58 -0500

    Update libcosmospm.c

--- libcosmospm.c
+++ libcosmospm.c
@@ -9,3 +9,4 @@ char version[MAX_LINE] = "UNKNOWN";
 char err_symbol[MAX_LINE] = "\e[0;31mE \e[0m>>>";
 char warn_symbol[MAX_LINE] = "\e[0;33mW \e[0m>>>";
 char info_symbol[MAX_LINE] = "\e[1;37mI \e[0m>>>";
+char cosmos_ver[MAX_LINE] = "v1.0.0-beta_release";
